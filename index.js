104a0afb8f5aa4fa83393ff26bd033a3d5481bd7
Matthew Mueller <mattmuelle@gmail.com>
2015-07-26 19:08:15 -0700

    better message

--- index.js
+++ index.js
@@ -86,8 +86,8 @@ app.post('/auth/local', function(req, res, next) {
  * Add a ping
  */
 
-app.get('/ping', function(req, res, next) {
-  res.send('PONG');
+app.get('/', function(req, res, next) {
+  res.send('bare auth running!');
 });
 
 /**
